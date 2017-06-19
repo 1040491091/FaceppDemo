d22277c7359b13b4786906f11b9b184615781b4e
guomudan <1040491091@QQ.com>
2017-06-19 17:47:39 +0800

    test

---
+++ FaceppDemoUI/src/main/java/com/facepp/library/util/Util.java
@@ -0,0 +1,9 @@
+package com.facepp.library.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "BB_HLDY173-3rnkSCedaaqcX9cTOtwaC";
+	public static String API_SECRET = "XwyvjTLDNbgwRF72RCvPGLx5yPrE8Ein";
+	
+}
